fa8b9946100e3b867ccb34641671b4868f6ed268
LIvanoff <drfrily@gmail.com>
2022-11-27 18:41:52 +0300

    update main

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     hm = HebbMethod()
     hm.train()
     #print(hm.weights[1])
-    integer_image = os.listdir('./integer')
+    integer_image = os.listdir('./digit')
     img_as_array = np.array([])
-    with Image.open('./integer/цифра 8.jpg') as img:
+    with Image.open('./digit/цифра 9.jpg') as img:
         img_as_array = np.append(img_as_array, img)
         for j in range(len(img_as_array)):
             if img_as_array[j] == 0 or img_as_array[j] == 1:
